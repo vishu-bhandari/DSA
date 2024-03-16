803f3c454bd55a90fa9e20d4c845a0804ccd1771
vishu bhandari <deevishu1234@gmail.com>
2024-03-16 21:24:30 +0530

    binary numbers

--- coding_lovebabbar/binary-decimal-no.system/binary-decimal.cpp
+++ coding_lovebabbar/binary-decimal-no.system/binary-decimal.cpp
@@ -2,20 +2,25 @@
 #include<math.h>
 using namespace std;
 
+
 int main(){
     int n;
     cout<<"enter the value of n"<<endl;
-    cin>>n;
 
-    int ans=0;
+    cin>>n;
     int i=0;
+    int ans=0;
+
     while(n!=0){
-        int digit=n%10;
-        if(digit==1){
+        int bit=n%10;
+        if(bit==1){
             ans=ans+pow(2,i);
         }
-        n=n/10;
         i++;
+        n=n/10;
     }
-    cout<<"the answer is "<<ans;
+    cout<<"the number is "<<ans<<endl;
+
+
+    return 0;
 }
--- coding_lovebabbar/binary-decimal-no.system/decimal-binary.cpp
+++ coding_lovebabbar/binary-decimal-no.system/decimal-binary.cpp
@@ -7,7 +7,17 @@ int main(){
     int n;
     cout<<"enter the value of n"<<endl;
     cin>>n;
-    
+
+    int ans=0;
+    int i=0;
+    while(n!=0){
+       int  bit=n&1;
+        ans=(bit*pow(10,i))+ans;
+
+        n=n>>1;
+        i++;
+    }
+    cout<<"the anser is "<<ans;
 
 
     return 0;

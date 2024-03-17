5d6ef2ff5dd6f248649e05bd982b4727bf1fabe2
vishu bhandari <deevishu1234@gmail.com>
2024-03-18 02:26:12 +0530

    functions

--- coding_lovebabbar/functions/Even-odd.cpp
+++ coding_lovebabbar/functions/Even-odd.cpp
@@ -1,29 +1,92 @@
 #include<iostream>
 using namespace std;
 
+bool evenOdd(int n){
 
-bool evenOdd(int a){
-    if(a%2==0){
-        return 1;
-
+    if(n%2==0){
+        return true;
+    }else{
+        return false;
     }
-    else{
-        return 0;
-    }
-
 }
-int main(){
-    int num;
-    cout<<"enter num"<<endl;
-    cin>>num;
 
-    if(evenOdd(num)){
+
+
+int main(){ 
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+
+    if(evenOdd(n)){
         cout<<"the number is even"<<endl;
+    }else{
+        cout<<"the number is odd"<<endl;
+    }
 
-    }
-    else{
-        cout<<"num is odd ";
-    }
+
+
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+
+
+// bool evenOdd(int a){
+//     if(a%2==0){
+//         return 1;
+
+//     }
+//     else{
+//         return 0;
+//     }
+
+// }
+// int main(){
+//     int num;
+//     cout<<"enter num"<<endl;
+//     cin>>num;
+
+//     if(evenOdd(num)){
+//         cout<<"the number is even"<<endl;
+
+//     }
+//     else{
+//         cout<<"num is odd ";
+//     }
+
+//     return 0;
+// }
--- coding_lovebabbar/functions/intro.power-of-a,b.cpp
+++ coding_lovebabbar/functions/intro.power-of-a,b.cpp
@@ -1,6 +1,6 @@
+
 #include<iostream>
 using namespace std;
-
 
 int power(int a,int b){
     int ans=1;
@@ -8,15 +8,73 @@ int power(int a,int b){
         ans=ans*a;
     }
     return ans;
-    
+
+
 }
+
 
 int main(){
     int a,b;
+    cout<<"enter the value of a and b"<<endl;
     cin>>a>>b;
-    int answer= power(a,b);
-    cout<<"answer is " << answer;
+
+    int answer=power(a,b);
+    cout<<"the asnwer is "<<answer<<endl;
+
+
+
 
     return 0;
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+// int power(int a,int b){
+//     int ans=1;
+//     for(int i=1;i<=b;i++){
+//         ans=ans*a;
+//     }
+//     return ans;
+    
+// }
+
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     int answer= power(a,b);
+//     cout<<"answer is " << answer;
+
+//     return 0;
+
+// }

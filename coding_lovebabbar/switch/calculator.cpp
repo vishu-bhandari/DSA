54b1b4593c457cea22ff80130af44d85f4bcef3e
vishu bhandari <deevishu1234@gmail.com>
2024-03-17 18:44:53 +0530

    calculatro using switch

--- coding_lovebabbar/switch/calculator.cpp
+++ coding_lovebabbar/switch/calculator.cpp
@@ -1,36 +1,106 @@
+
 #include<iostream>
 using namespace std;
 
+
 int main(){
-    int a,b;
-    cout<<"Enter the value of a"<<endl;
-    cin>>a;
-    cout<<"enter the value of b"<<endl;
-    cin>>b;
+        int a,b;
+        cout<<"enter the value of a"<<endl;
+        cin>>a;
+        cout<<"enter the value of b"<<endl;
+        cin>>b;
 
-    char op;
-    cout<<"enter the operation "<<endl;
-    cin>>op;
-    cout<<endl;
+        //operation
+        char opr;
+        cout<<"enter the arithmetic operation"<<endl;
+        cin>>opr;
 
-    switch(op){
-        case '*':cout<<"the value of a*b is "<<a*b<<endl;
+        switch (opr)
+        {
+        case '*':
+               cout<<"value of a*b is "<<a*b<<endl;
                 break;
-        case '+':cout<<a+b<<endl;
+        case '+':
+               cout<<"value of a+b is "<<a+b<<endl;
                 break;
-        case '%':cout<<a%b<<endl;
+        case '-':
+               cout<<"value of a-b is "<<a-b<<endl;
                 break;
-        case '-':cout<<a-b<<endl; 
+        case '/':
+               cout<<"value of a/b is "<<a/b<<endl;
                 break;
+        case '%':
+               cout<<"value of a%b is "<<a%b<<endl;
+                break;
+        default:
+        cout<<"this is the default case"<<endl;
+        break;
+        
+        }
 
-        default:cout<<"enter a valid operation"<<endl;
-                break;
 
-    }
+        return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a,b;
+//     cout<<"Enter the value of a"<<endl;
+//     cin>>a;
+//     cout<<"enter the value of b"<<endl;
+//     cin>>b;
+
+//     char op;
+//     cout<<"enter the operation "<<endl;
+//     cin>>op;
+//     cout<<endl;
+
+//     switch(op){
+//         case '*':cout<<"the value of a*b is "<<a*b<<endl;
+//                 break;
+//         case '+':cout<<a+b<<endl;
+//                 break;
+//         case '%':cout<<a%b<<endl;
+//                 break;
+//         case '-':cout<<a-b<<endl; 
+//                 break;
+
+//         default:cout<<"enter a valid operation"<<endl;
+//                 break;
+
+//     }
     
 
 
 
 
-    return 0;
-}
+//     return 0;
+// }

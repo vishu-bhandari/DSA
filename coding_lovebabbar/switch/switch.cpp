645442785e6c647e4d0204668276e8c7f3cda458
vishu bhandari <deevishu1234@gmail.com>
2024-03-17 18:29:07 +0530

    switch

--- coding_lovebabbar/switch/switch.cpp
+++ coding_lovebabbar/switch/switch.cpp
@@ -1,15 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    char ch='a';
-    cout<<endl;
-    switch(ch){
-        case 1:cout<<"first"<<endl;
-            break;
-        case '1':cout<<"character one "<<endl;
-            break;
-        default: cout<<"it is a default case "<<endl;
+int main()
+{
+    char ch = 'a';
+    cout << endl;
+    switch (ch)
+    {
+    case 1:
+        cout << "first" << endl;
+        break;
+    case '1':
+        cout << "character one " << endl;
+        break;
+    case 'a':
+        cout << "character a" << endl;
+        break;
+    case 2:
+        cout << "this is a number" << endl;
+        break;
+    default:
+        cout << "it is a default case " << endl;
     }
-    cout<<endl;
+    cout << endl;
 }

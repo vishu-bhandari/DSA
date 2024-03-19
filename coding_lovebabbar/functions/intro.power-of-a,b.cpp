b2742316ac8b9ecffe70eec15b66d6caa89bd1ef
vishu bhandari <deevishu1234@gmail.com>
2024-03-19 20:12:06 +0530

    c

--- coding_lovebabbar/functions/intro.power-of-a,b.cpp
+++ coding_lovebabbar/functions/intro.power-of-a,b.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-int power(int a,int b){
+int power(int c,int b){
     int ans=1;
     for(int i=1;i<=b;i++){
         ans=ans*a;
@@ -55,26 +55,3 @@ int main(){
 
 
 
-
-// #include<iostream>
-// using namespace std;
-
-
-// int power(int a,int b){
-//     int ans=1;
-//     for(int i=1;i<=b;i++){
-//         ans=ans*a;
-//     }
-//     return ans;
-    
-// }
-
-// int main(){
-//     int a,b;
-//     cin>>a>>b;
-//     int answer= power(a,b);
-//     cout<<"answer is " << answer;
-
-//     return 0;
-
-// }

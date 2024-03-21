31b92e337779f21d95199c2bb5b412ee7be99cbf
vishu bhandari <deevishu1234@gmail.com>
2024-03-21 23:21:09 +0530

    functions

--- coding_lovebabbar/functions/counting.cpp
+++ coding_lovebabbar/functions/counting.cpp
@@ -1,20 +1,78 @@
+
 #include<iostream>
 using namespace std;
 
-// function signature 
-void printCounting(int n){
-    // function body 
-     for(int i=1;i<=n;i++){
+void count(int n){
+
+    for(int i=1;i<=n;i++){
         cout<<i<<" ";
-     }
+    }
+    
 }
 
+
+
 int main(){
+
+
     int n;
     cout<<"enter the value of n"<<endl;
     cin>>n;
-    // function call 
-    printCounting(n);
-    cout<<endl;
 
+    count(n);
+
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// // function signature 
+// void printCounting(int n){
+//     // function body 
+//      for(int i=1;i<=n;i++){
+//         cout<<i<<" ";
+//      }
+// }
+
+// int main(){
+//     int n;
+//     cout<<"enter the value of n"<<endl;
+//     cin>>n;
+//     // function call 
+//     printCounting(n);
+//     cout<<endl;
+
+// }
--- coding_lovebabbar/functions/ncr.cpp
+++ coding_lovebabbar/functions/ncr.cpp
@@ -2,29 +2,96 @@
 using namespace std;
 
 
-
 int factorial(int n){
-    int fact=1;
+    int a=1;
     for(int i=1;i<=n;i++){
-        fact=fact*i;
+        a=a*i;
+
     }
-    return fact;
+    return a;
 }
-
-
 int nCr(int n,int r){
     int num=factorial(n);
     int den=factorial(r)*factorial(n-r);
 
-    int ans=num/den;
-    return ans;
+    int answer=num/den;
+    return answer;
 }
+
+
 
 int main(){
     int n,r;
-    cout<<"enter the value of n and r "<<endl;
-
+    cout<<"enter the value of n and r"<<endl;
     cin>>n>>r;
-    cout<<"the answer is"<<nCr(n,r);
 
+
+    int ans=nCr(n,r);
+    cout<<ans<<endl;
+
+    
+
+
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+
+// int factorial(int n){
+//     int fact=1;
+//     for(int i=1;i<=n;i++){
+//         fact=fact*i;
+//     }
+//     return fact;
+// }
+
+
+// int nCr(int n,int r){
+//     int num=factorial(n);
+//     int den=factorial(r)*factorial(n-r);
+
+//     int ans=num/den;
+//     return ans;
+// }
+
+// int main(){
+//     int n,r;
+//     cout<<"enter the value of n and r "<<endl;
+
+//     cin>>n>>r;
+//     cout<<"the answer is"<<nCr(n,r);
+
+// }

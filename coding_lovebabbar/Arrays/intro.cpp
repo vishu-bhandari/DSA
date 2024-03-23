2a70fa4c320192f13c2ec2d2faf432cb30935ccc
vishu bhandari <deevishu1234@gmail.com>
2024-03-23 19:05:58 +0530

    arrays

--- coding_lovebabbar/Arrays/intro.cpp
+++ coding_lovebabbar/Arrays/intro.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void printArray(int n){
+void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"printing done"<<endl;
 
 }
 
@@ -11,54 +15,61 @@ int main(){
     int num[15];
 
     // accessing an array 
-    cout<<"value of num at 14 is "<<num[14]<<endl;
+    cout<<"the value of num at 14 is "<<num[14]<<endl;
 
     // cout<<"value of num at 20 is "<<num[20]<<endl;
 
 
     // initialising an array 
-    int second[3]={5,7,11};
+   int second[3]={5,7,11};
 
 
     //accessing an element 
-    cout<<second[2]<<endl;
+    cout<<"the value at second[3] is "<<second[2]<<endl;
 
 
     int third[15]={2,7};
     
 
     int n=15;
-    cout<<"printing the array "<<endl;
-    //printing the array 
-    for(int i=0;i<n;i++){
-        cout<<third[i]<<" ";
-    }
+    // cout<<"printing the array "<<endl;
+    // //printing the array 
+    // for(int i=0;i<n;i++){
+    //     cout<<third[i]<<" ";
+    // }
+    printArray(third,15);
 
     cout<<endl;
 
     // initialising all locations with zero 
     int fourth[10]={0};
-    int nb=10;
-    for(int i=0;i<nb;i++){
-        cout<<fourth[i]<<" ";
-    }
+    // int nb=10;
+    // for(int i=0;i<nb;i++){
+    //     cout<<fourth[i]<<" ";
+    // }
+    printArray(fourth,10);
     cout<<endl;
 
     int fifth[10]={1};
-    n=10;
-    for(int i=0;i<n;i++){
-        cout<<fifth[i]<<" ";
-    }
+    // n=10;
+    // for(int i=0;i<n;i++){
+    //     cout<<fifth[i]<<" ";
+    // }
+    printArray(fifth,10);
+    int fifthsize=sizeof(fifth)/sizeof(int);
+    cout<<"size of fifth is "<<fifthsize;
     cout<<endl;
 
-    int sixth[10];
-    n=10;
-    for(int i=0;i<n;i++){
-        sixth[i]=5;
-        cout<<sixth[i]<<" ";
-    }
+   int sixth[10];
+    // int ns=10;
+    // for(int i=0;i<=ns;i++){
+    //     sixth[i]=5;
+    //     cout<<sixth[i]<<" ";
+    // }
+    printArray(sixth,10);
 
 
+    
 
 
 

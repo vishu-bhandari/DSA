6591b921c7351149b94e1a6113d6a115783fbadb
vishu-bhandari <deevishu1234@gmail.com>
2024-08-16 18:33:51 +0530

    BS even odd

--- coding_lovebabbar/dsa-binarysearch-lec12/binary-search.cpp
+++ coding_lovebabbar/dsa-binarysearch-lec12/binary-search.cpp
@@ -1,38 +1,37 @@
 #include<iostream>
 using namespace std;
 
-int binarySearch(int arr[],int size,int key){
-    int start=0 ;
+int BinarySearch(int arr[], int size,int key){
+    int start=0;
     int end=size-1;
-    int mid=(start + end )/2;
+
+    int mid=start + (end-start)/2;
 
     while(start<=end){
         if(arr[mid]==key){
             return mid;
         }
-        // going to right wla part 
         if(key>arr[mid]){
             start=mid+1;
+
         }
         else{
             end=mid-1;
         }
-        mid=(start+end)/2;
+        mid=start + (end-start)/2;
     }
     return -1;
 }
 
-int main()
-{
+int main(){
+
     int even[6]={2,4,6,8,12,18};
     int odd[5]={3,8,11,14,16};
 
-    int evenindex=binarySearch(even,6,18);
-    cout<<"index of 12 is "<< evenindex <<endl;
-    
-
-    int oddindex=binarySearch(odd,5,20);
-    cout<<"index of 20 is "<< oddindex <<endl;
+    int index=BinarySearch(even,6,18);
+    cout<<"Index of 12 is "<<index<<endl;
+    int index1=BinarySearch(odd,5,20);
+    cout<<"Index of 20 is "<<index1<<endl;
 
 
 

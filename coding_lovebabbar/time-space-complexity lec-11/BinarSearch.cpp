acaa43d21d18e6a5d4fc67f5772f954a7db0dbe6
vishu-bhandari <deevishu1234@gmail.com>
2024-08-17 23:27:03 +0530

    bs peak index, and first and last pos of elem

---
+++ coding_lovebabbar/time-space-complexity lec-11/BinarSearch.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int size,int key){
+    int start=0;
+    int end=size-1;
+
+    int mid=start + (end-start)/2;
+
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(key>arr[mid]){
+            start=mid+1;
+
+        }
+        else{
+            end=mid-1;
+        }
+        mid=start + (end-start)/2;
+    }
+    return -1;
+}
+//n size , then n/2 size , then n/4 , then n/8 .....then
+//n/2^k aur n/2^k =1 toh n=2^k , k=logn 
+
+int main(){
+
+    int even[6]={2,4,6,8,12,18};
+    int odd[5]={3,8,11,14,16};
+
+    int index=BinarySearch(even,6,18);
+    cout<<"Index of 12 is "<<index<<endl;
+    int index1=BinarySearch(odd,5,20);
+    cout<<"Index of 20 is "<<index1<<endl;
+
+
+
+
+    return 0;
+}

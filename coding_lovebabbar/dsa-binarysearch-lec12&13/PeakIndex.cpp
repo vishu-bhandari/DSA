0b742f48f767b97d0b9a682e002c13a0f95d1bf6
vishu-bhandari <deevishu1234@gmail.com>
2024-08-18 22:59:01 +0530

    PeakIndex

--- coding_lovebabbar/dsa-binarysearch-lec12&13/PeakIndex.cpp
+++ coding_lovebabbar/dsa-binarysearch-lec12&13/PeakIndex.cpp
@@ -1,9 +1,33 @@
 #include<iostream>
 using namespace std;
 
+int PeakIndex(int arr[],int n ){
+
+    int start=0;
+    int end=n-1;
+    int mid=start + (end-start)/2;
+
+    while(start<end){
+        if(arr[mid]<arr[mid+1]){
+            start=mid+1;
+        }
+        else{
+            end=mid;
+        }
+
+        mid=start + (end-start)/2;
+    }
+    return start;
+
+}
+
 
 int main(){
     
+    int arr[5]={1 ,2 ,4 ,5, 3 };
+
+    cout<<PeakIndex(arr,5);
+
 
     return 0;
 }

f039c4970b0b964ff979a045addb6fe01355ddff
vishu-bhandari <deevishu1234@gmail.com>
2024-10-13 14:48:24 +0530

    stacks

--- coding_lovebabbar/stacks Lec54-59/Lec 54/Lec54-introduction.cpp
+++ coding_lovebabbar/stacks Lec54-59/Lec 54/Lec54-introduction.cpp
@@ -2,67 +2,71 @@
 using namespace std;
 #include <stack>
 
-class Stack
-{
-    // properties
-public:
-    int *arr;
-    int top;
-    int size;
+class Stack{
+    //properties 
+    public:
+        int *arr;
+        int top;
+        int size;
 
-    // behaviour
 
-    Stack(int size){
-        this->size=size;
-        arr=new int[size];
-        top=-1;
-    }
-
-    void push(int element){
-        if(size-top>1){
-            top++;
-            arr[top]=element;
-        }else{
-            cout<<"stack overflowed"<<endl;
-        }
-    }
-
-    void pop(){
-        if(top>=0){
-            top--;
-        }else{
-            cout<<"stack underflow"<<endl;
+        //behaviour
+        Stack(int size){
+            this->size=size;
+            arr=new int[size];
+            top=-1;
         }
 
-    }
-
-    int peak(){
-
-        if(top>=0 && top<size){
-
-        return arr[top];
-        }else{
-            cout<<"stack is empty ";
-            return -1;
+        void push(int element){
+            if(size-top>1){
+                top++;
+                arr[top]=element;
+            }else{
+                cout<<"Stack overflow"<<endl;
+            }
         }
+        void pop(){
+            if(top>=0){
+                top--;
+            }else{
+                cout<<"Stack underflow"<<endl;
+            }
 
-    }
-
-    bool isEmpty(){
-        if(top==-1){
-            return true;
-        }else{
-            false;
         }
-    }
+        int peak(){
+            if(top>=0 && top<size){
+
+            return arr[top];
+            }else{
+                cout<<"stack is empty "<<endl;
+            }
+
+        }
+        bool isEmpty(){
+            if(top==-1){
+                return true;
+            }else{
+                return false;
+            }
+        }
 };
+
+
 
 int main()
 {
 
-    stack st(5);
+    Stack st(5);
 
-    // stack<int> s;....
+    st.push(22);
+    st.push(23);
+    st.push(24);
+    st.push(25);
+
+    cout<<st.peak()<<endl;
+
+
+    // stack<int> s;
 
     // s.push(2);
     // s.push(3);
